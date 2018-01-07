99e0c122214649d6a9e55155ea9f097a5b96a1b8
Tucker <tuckerk23@gmail.com>
2018-01-07 11:20:22 -0700

    updated logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,30 @@
-
 //this variable stores the original items and queried items to be displayed
-var buttonArr = ['cat', 'dog', 'mouse', 'frog', 'chimpanzee'];
+var buttonArr = ["spongebob", "archer", "southpark", "adventure time", "doug", "rick and morty",  "bobs burgers", "duck tales", "rockos modern life", "futurama"];
 
 //empties the button container to avoid conflicts upon refreshing
-$('.btnContainer').empty();
+$(".btnContainer").empty();
 
 //generates buttons for the original items in array and future queried buttons
-function generateButtons(){
+function generateButtons() {
 
-//loops through array to generate buttons, add attributes, and add classes for each arr item
-for (let i = 0; i < buttonArr.length; i++) {
-    
+  //loops through array to generate buttons, add attributes, and add classes for each arr item
+  for (let i = 0; i < buttonArr.length; i++) {
+      
     //variable using jquery to create a button
-    var animalButton = $('<button>');
+    var cartoonButton = $("<button>");
 
     //adds classes to each button
-    animalButton.addClass('animal btn-danger btn-lg');
+    cartoonButton.addClass("animal btn-danger btn-lg");
 
     //adds the 'data-name' attribute to each button based on its array index/name
-    animalButton.attr('data-name', buttonArr[i]);
+    cartoonButton.attr("data-name", buttonArr[i]);
 
     //adds text/title to each button based on its array index/name
-    animalButton.text(buttonArr[i]);
+    cartoonButton.text(buttonArr[i]);
 
     //appends buttons for each item in the array
-    $('.btnContainer').append(animalButton);
-};
+    $(".btnContainer").append(cartoonButton);
+  }
 }
 
 generateButtons();
